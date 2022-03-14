1249bf855e24881881604b439a6968bfbded789a
VIJETA VAMAN NAYAK <vijeta109@gmail.com>
2022-03-14 12:01:20 -0400

    database connectivity full

---
+++ scripts/driver/driver.py
@@ -0,0 +1,22 @@
+from linkedInutility import open_config
+from linkedInutility import open_logger
+from liveData import livedata
+import sys
+import logging
+
+# import datetime as dt
+# import json
+# import os
+import logging
+
+from linkedInutility import get_db_connection
+from company_master import company_master_data
+
+cfg = open_config(sys.argv)
+
+open_logger(cfg["log_file"])
+logging.info(cfg)
+livedata(cfg)
+company_master_data(cfg)
+
+
---
+++ scripts/shell/executeProcess.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=${PYTHONPATH}:/Users/vijetavamannayak/LinkedInProject/scripts/utility/
+export PYTHONPATH=${PYTHONPATH}:/Users/vijetavamannayak/LinkedInProject/scripts/master/
+python3 /Users/vijetavamannayak/LinkedInProject/scripts/driver/driver.py config_file=/Users/vijetavamannayak/LinkedInProject/scripts/driver/driver.json
